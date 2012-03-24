75a9a3e0418eea986e60d0e0b6aad05bdc22eafe
deadbok <martin.groenholdt@gmail.com>
2012-03-24 15:56:21 +0100

    Working on node saving

--- EdenLinux/config/writer.py
+++ EdenLinux/config/writer.py
@@ -6,6 +6,7 @@ import log
 import os.path
 import configoption
 import configtree
+from bike.transformer.save import save
 
 
 class Writer(object):
@@ -41,20 +42,35 @@ class Writer(object):
         while done == False:
             #No open file
             filename = ""
+            config_file = None
             #Run through nodes
             for node in self.config_tree.iter():
                 #Get a filename if it is empty
                 if filename == "":
                     if node.filename not in filenames:
                         filename = node.filename
+                        config_file = open(filename, "rw")
                         log.logger.debug("Saving entries to file: " + filename)
-
                 #Save entry if it is owned
                 if filename != "":
                     if node.filename == filename:
                         log.logger.debug("Saving entry: " + node.name)
+                        self.save_node(node, config_file)
             #If no file was found, we're done, else save the filename.
             if filename == "":
                 done = True
             else:
                 filenames.append(filename)
+
+    def save_node(self, node, config_file):
+        '''
+        Save a node to a file.
+        
+        @param node: Node to save
+        @type node: L{ConfigOption}
+        @param config_file: File to save the node to
+        @type config_file: file
+        '''
+        node_str = list()
+        node_str.append("##CONFIG: " + node.name)
+        log.logger.debug("Looking for: " + node_str[0])
